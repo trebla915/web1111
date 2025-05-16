449dada8c9b58a44369dc0949dc5917917f0f612
trebla915 <tr3bla915@gmail.com>
2025-05-15 23:17:18 -0600

    Expo 53 updgrade

--- metro.config.js
+++ metro.config.js
@@ -1,35 +1,32 @@
 const { getDefaultConfig } = require('@expo/metro-config');
 const path = require('path');
 
-const defaultConfig = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-defaultConfig.resolver.sourceExts = [
-  ...defaultConfig.resolver.sourceExts,
+config.resolver.unstable_enablePackageExports = false;
+
+config.resolver.sourceExts = [
+  ...config.resolver.sourceExts,
   'web.ts',
   'web.tsx',
   'web.js',
   'web.jsx',
-  'cjs' // Ensure 'cjs' is included
+  'cjs',
 ];
 
-defaultConfig.resolver.alias = {
+config.resolver.alias = {
   '@': path.resolve(__dirname, './'),
   '@/assets': path.resolve(__dirname, './src/assets'),
   '@/images': path.resolve(__dirname, './src/assets/images'),
   '@/config': path.resolve(__dirname, './src/config'),
   '@/contexts': path.resolve(__dirname, './src/contexts'),
-  '@/utils': path.resolve(__dirname, './src/utils')
+  '@/utils': path.resolve(__dirname, './src/utils'),
 };
 
-defaultConfig.resolver.extraNodeModules = {
-  ...defaultConfig.resolver.extraNodeModules,
-  'react-native': require.resolve('react-native-web') // Map 'react-native' to 'react-native-web'
-};
-
-defaultConfig.watchFolders = [
+config.watchFolders = [
   path.resolve(__dirname, 'node_modules/react-native-web'),
-  path.resolve(__dirname, 'src'), // Add src folder to watch
-  ...defaultConfig.watchFolders
+  path.resolve(__dirname, 'src'),
+  ...config.watchFolders,
 ];
 
-module.exports = defaultConfig;
+module.exports = config;
